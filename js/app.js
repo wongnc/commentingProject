f9c0d23a08d78d115c9b6a070d4e66c3413fbf61
Nicholas Wong <wong.nc@gmail.com>
2017-04-20 01:13:02 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+//add user input to a list
+
+function addComment(){
+	//store user name input
+	var userName = document.getElementById("userName").value;
+	//store user comment input
+	var userInput = document.getElementById("userComment").value;
+	//current time and date comment was posted
+	var br = document.createElement('br');
+
+	//create li element
+	var li = document.createElement("li");
+	//create class for li to be edited in css
+	li.className = "liClass"
+	//add user name and comment to the li element
+	li.innerHTML = userName + ":" + "</br>" + userInput;
+	//put li in my ul element on screen
+	document.getElementById("comments").appendChild(li);
+	//clear out the input box
+	document.getElementById("userName").value = "";
+	document.getElementById("userComment").value = "";
+
+}
+
+
+
+// var d = new Date();
+// document.getElementById("demo").innerHTML = d.toDateString();
